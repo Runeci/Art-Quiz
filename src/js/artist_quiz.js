e01a8828c55e1ce548977b84086c6fba159adde7
Dariya <77978099+Runeci@users.noreply.github.com>
2021-11-13 21:27:43 +0300

    feat: add artist quiz page

---
+++ src/js/artist_quiz.js
@@ -0,0 +1,202 @@
+import {createBtnScore, loadJson} from "./artist";
+import {buttonCategoryArtist, buttonHome, makeVisible, removeClass} from "./main";
+
+export const numberOfQuestions = 10;
+
+loadJson('./assets/json.json').catch(alert).then(data => {
+    let infoArr = data.items;
+    const cardArr = document.querySelectorAll('.artist-card') // arr with all category cards
+    const btnNext = document.querySelector('.artist-quiz__next')
+    const quizAnswersContainer = document.querySelector('.artist-quiz__answers')
+    const quizAnswerContainersArr = document.querySelectorAll('.artist-quiz__answer')
+    const modalContent = document.querySelector('.modal-inner-text') // modal with right answer
+    const quizQuestionsNumArr = document.querySelectorAll('.artist-quiz__question-number')
+    const modalNext = document.getElementById("myModal");
+    const modalResults = document.querySelector('.modal-results');
+    const modalResultsScore = document.querySelector('.modal-results__score');
+    const btnResult = document.querySelector('.modal-results__next')
+    const arrayParts = [];
+    let arrWithResults = [];
+    let correctAnswer; //correct answer
+    let score = 0; // total score
+    let currImgNum = 0; // current number of img
+    let currCategoryNum; // current category card number
+    let numberOfAnsweredQuestions = 0;
+
+    divideIntoParts(infoArr, arrayParts);
+
+    cardArr.forEach((card, i) => {
+        if (localStorage.getItem(`visited ${i}`) === 'true') {
+            createBtnScore(cardArr, i)
+        }
+
+    })
+
+    cardArr.forEach((card, index) => {
+        card.addEventListener('click', () => {
+            currCategoryNum = index;
+            setFirstQuizImg(arrayParts, index)
+            getAnswers(arrayParts, currCategoryNum, currImgNum);
+        })
+    })
+
+    btnNext.addEventListener('click', () => {
+        setNumberOfCurrentImg();
+        getAnswers(arrayParts, currCategoryNum, currImgNum);
+        setImg(arrayParts, currCategoryNum, currImgNum)
+        removeClass(quizAnswerContainersArr, 'correct')
+        removeClass(quizAnswerContainersArr, 'wrong')
+        hideModal(modalNext)
+
+        localStorage.setItem(`card artist ${currCategoryNum}`, `${score}`) //set score
+
+        // localStorage.setItem(`artist results ${currCategoryNum}`, `${arrWithResults}`)
+        quizAnswersContainer.classList.remove('disabled')
+        numberOfAnsweredQuestions++
+        if (numberOfAnsweredQuestions === numberOfQuestions) {
+            showModal(modalResults)
+            modalResultsScore.innerHTML = `${score} / ${numberOfQuestions}`
+        }
+
+        if (localStorage.getItem(`visited ${currCategoryNum}`) === 'true') {
+            createBtnScore(cardArr, currCategoryNum)
+        }
+    })
+
+
+    btnResult.addEventListener('click', () => {
+        makeVisible('artist-category')
+        setDefaultValues()
+    })
+
+    buttonCategoryArtist.addEventListener('click', () => {
+        if (arrWithResults.length < numberOfQuestions) {
+            for (let i = arrWithResults.length; i < numberOfQuestions; i++) {
+                arrWithResults.push('false')
+                console.log(arrWithResults.length, 'length' )
+                console.log(arrWithResults)
+            }
+
+        }
+        localStorage.setItem(`artist results ${currCategoryNum}`, `${arrWithResults}`)
+        setDefaultValues();
+        createBtnScore(cardArr, currCategoryNum)
+    })
+
+
+    function setDefaultValues() {
+        hideModal(modalResults)
+        removeClass(quizQuestionsNumArr, 'correct')
+        removeClass(quizQuestionsNumArr, 'wrong')
+        score = 0;
+        numberOfAnsweredQuestions = 0;
+        currImgNum = 0;
+        arrWithResults = [];
+    }
+
+
+    function setNumberOfCurrentImg() {
+        currImgNum++
+        if (currImgNum === numberOfQuestions) {
+            currImgNum = 0;
+        }
+    }
+
+    function setFirstQuizImg(arr, index) {
+        const img = document.querySelector('.artist-quiz__img');
+        img.style.backgroundImage = `url("./assets/images/img/${arr[index][0].imageNum}.jpg")`
+    }
+
+    function setImg(arr, index, num) {
+        const img = document.querySelector('.artist-quiz__img');
+        img.style.backgroundImage = `url("./assets/images/img/${arr[index][num].imageNum}.jpg")`
+    }
+
+    function getAnswers(partArr, arrNumber, imageNum) {
+        const maxIndex = infoArr.length - 1;
+        const numberOfAnswers = 4;
+        correctAnswer = partArr[arrNumber][imageNum].author;
+
+        let answersArr = [correctAnswer];
+        for (let i = 0; i < numberOfAnswers - 1; i++) {
+            let answer = infoArr[getRandomNum(0, maxIndex)].author
+            if (answer !== correctAnswer) {
+                answersArr.push(answer);
+            } else {
+                answersArr.push(infoArr[getRandomNum(0, maxIndex)].author)
+            }
+        }
+
+        shuffle(answersArr);
+
+        quizAnswerContainersArr.forEach((container, index) => {
+            container.innerHTML = `${answersArr[index]}`
+        })
+
+        checkAnswer()
+    }
+
+
+    quizAnswersContainer.addEventListener('click', e => { // get total score
+        if (e.target.innerHTML === correctAnswer) {
+            score++
+            arrWithResults.push('true')
+            quizQuestionsNumArr[currImgNum].classList.add('correct')
+
+        } else {
+            arrWithResults.push('false')
+            quizQuestionsNumArr[currImgNum].classList.add('wrong')
+        }
+    })
+
+    function checkAnswer() {
+        quizAnswerContainersArr.forEach(cont => {
+            cont.addEventListener('click', () => {
+                if (cont.innerHTML === correctAnswer) {
+                    cont.classList.add('correct')
+                } else {
+                    cont.classList.add('wrong')
+                }
+
+                modalContent.textContent = `правильный ответ: ${correctAnswer}`
+                quizAnswersContainer.classList.add('disabled')
+
+                showModal(modalNext)
+
+            })
+        })
+    }
+
+});
+
+
+export function divideIntoParts(arr, newArr) {
+    const copyArr = [].concat(arr)
+    while (copyArr.length > 0)
+        newArr.push(copyArr.splice(0, 10));
+}
+
+export function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+export function getRandomNum(min, max) {
+    let randomNum
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+    return randomNum;
+}
+
+
+export function hideModal(modal) {
+    modal.style.top = "-100%";
+}
+
+export function showModal(modal) {
+    modal.style.top = "0";
+}
+
